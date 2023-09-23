8ad2e3d97479392c5564e3633a24ab0490fd0982
Debayan Pratihar <121422042+debayanpratihar@users.noreply.github.com>
2023-09-23 23:09:34 +0530

    third update

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json({ limig: "50mb" }))
 app.use("/api/v1/dalle", dalleRoutes);
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: "Hello from DEBAYAN" })
+  res.status(200).json({ message: "Hello from Debayan" })
 })
 
 app.listen(8080, () => console.log('Server has started on port 8080'))
--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -13,7 +13,7 @@ const config = new Configuration({
 const openai = new OpenAIApi(config);
 
 router.route('/').get((req, res) => {
-  res.status(200).json({ message: "Hello from Debayan ROUTES" })
+  res.status(200).json({ message: "Hello from Debayan" })
 })
 
 router.route('/').post(async (req, res) => {
