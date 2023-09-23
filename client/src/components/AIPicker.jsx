0a99363e4fd2517856034a52c86c607577fc16d5
Debayan Pratihar <121422042+debayanpratihar@users.noreply.github.com>
2023-09-23 22:48:05 +0530

    update the server url

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -16,7 +16,7 @@ const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
         {generatingImg ? (
           <CustomButton 
             type="outline"
-            title="Asking AI..."
+            title="Asking Debayan..."
             customStyles="text-xs"
           />
         ) : (
