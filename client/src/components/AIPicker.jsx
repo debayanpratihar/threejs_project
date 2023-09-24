133d8c3a8dddfa94aca94a8230bba52f6888b98d
Debayan Pratihar <121422042+debayanpratihar@users.noreply.github.com>
2023-09-24 09:29:25 +0530

    updating new with node

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -6,7 +6,7 @@ const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
   return (
     <div className="aipicker-container">
       <textarea 
-        placeholder="Ask AI..."
+        placeholder="Ask  Debayan AI..."
         rows={5}
         value={prompt}
         onChange={(e) => setPrompt(e.target.value)}
--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json({ limig: "50mb" }))
 app.use("/api/v1/dalle", dalleRoutes);
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: "Hello from Debayan" })
+  res.status(200).json({ message: "Hello from DALL.E" })
 })
 
 app.listen(8080, () => console.log('Server has started on port 8080'))
