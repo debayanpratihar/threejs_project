3304f5b90eabdd44d0f559082896a13e1fb3d070
Debayan Pratihar <121422042+debayanpratihar@users.noreply.github.com>
2023-10-11 20:16:12 +0530

    update the backend url

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,9 @@
 const config = {
   development: {
-    backendUrl: "https://debayan-dress-designer-ai.onrender.com/api/v1/dalle",
+    backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://debayan-dress-designer-ai.onrender.com/api/v1/dalle",
+    backendUrl: "http://localhost:8080/api/v1/dalle",
   },
 };
 
